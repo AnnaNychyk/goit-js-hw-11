ff95d10f9ec93aec1105beabc050dca5637ec4a6
AnnaNychyk <annanichichka@gmail.com>
2022-03-14 16:56:49 +0200

    do task 11

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -12,4 +12,41 @@ export default async function getImages(word) {
         // console.error(error);
         } 
 }
-  
+
+// export default class apiService {
+//   constructor() {
+//     this.name = '';
+//     this.API_KEY = API_KEY;
+//     this.BASE_URL = BASE_URL;
+//     this.page = 1;
+//   }
+
+//   async fetchArticles(page) {
+//     try {
+//       const response = await axios.get(
+//         `${this.BASE_URL}?key=${this.API_KEY}&q=${this.name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`,
+//       );
+//       const articles = await response.data;
+//       //   this.incrementPage();
+//       return articles;
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+//   incrementPage() {
+//     this.page += 1;
+//   }
+
+//   resetPage() {
+//     this.page = 1;
+//   }
+
+//   get searchName() {
+//     return this.name;
+//   }
+
+//   set searchName(newName) {
+//     this.name = newName;
+//   }
+// }
